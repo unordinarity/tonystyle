259ea276e7a95847e7d0fbed1b62c1454f44e20e
unordinarity <unordinarity@yandex.ru>
2023-02-07 10:00:55 +0300

    all buggy stuff is now gitignored

--- packages/tonystyle/src/pages/projects/page.tsx
+++ packages/tonystyle/src/pages/projects/page.tsx
@@ -12,12 +12,16 @@ const Projects: FunctionComponent = () => (
       <h2>Feature-Sliced Design</h2>
 
       <h2>Panopticon</h2>
+      <p>React component gallery as React component</p>
 
       <h2>Turntable</h2>
+      <p>Framework-agnostic isomorphic architecture-friendly SPA router</p>
 
-      <h2>Tabea</h2>
+      <h2>Tabletka</h2>
+      <p>Beautiful and stylish new tab for Chrome and Firefox</p>
 
       <h2>Tony.Style</h2>
+      <p>This website. Business card, CV, personal blog, petproject playground, etc</p>
     </PageTemplate>
   </>
 )
